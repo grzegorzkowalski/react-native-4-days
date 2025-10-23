79fd3b762154808aebccb7405c8e12f6d4e9f97c
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-10-23 11:16:52 +0200

    update APIS project to expo 54

---
+++ Workspace/cricket-app/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import PlayerList from '@/components/PlayerList';
+
+export default function HomeScreen() {
+  return (
+      <>
+        <Stack.Screen
+            options={{
+              title: 'Indian Cricket Team',
+            }}
+        />
+        <PlayerList />
+      </>
+  );
+}
